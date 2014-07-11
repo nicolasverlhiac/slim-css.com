9c09826d0fcfb3705739edba121087a7379c909f
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2014-07-11 14:58:19 +0200

    ADD - Favicon
    New favicon for IOS, Browser, Windows 8.

--- header.php
+++ header.php
@@ -3,6 +3,11 @@ $f = basename($_SERVER['PHP_SELF']);
 include_once ("config.php");
 ?>
 <!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <html lang="fr">
 
 	<head>
@@ -22,9 +27,28 @@ include_once ("config.php");
 		<meta property="og:description" content="Slim HTML/CSS est un framework responsive, léger, et prêt à l'emploi."/>
 		<meta name="viewport" content="initial-scale=1,minimum-scale=1,width=device-width">
 
+		<!-- Icones pour le site --> 
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+		<meta name="apple-mobile-web-app-title" content="Slim CSS Framework">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="img/favicons/mstile-144x144.png">
+		<meta name="application-name" content="Slim CSS Framework">
+
 		<script src="code/prettify.js"> </script> 
 		<link href="code/prettify.css" rel="stylesheet" type="text/css" media="screen" />
-
+		
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="css/slim-icons.css" type="text/css" media="screen" />
 		<link rel="stylesheet" type="text/css" href="css/slim.css" />
@@ -32,12 +56,6 @@ include_once ("config.php");
 		<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
 
 		<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js"></script> -->
-
-		<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-		<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-		<!--[if IE 8]> <html class="no-js lt-ie9"><![endif]-->
-		<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
-		<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	</head>
 
 	<body onload="prettyPrint()">
--- index.php
+++ index.php
@@ -3,6 +3,11 @@ $f = basename($_SERVER['PHP_SELF']);
 include_once ("config.php");
 ?>
 <!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <html lang="fr">
 
 	<head>
@@ -21,18 +26,30 @@ include_once ("config.php");
 		<meta property="og:site_name" content="Slim HTML/CSS"/> 
 		<meta property="og:description" content="Slim HTML/CSS est un framework responsive, léger, et prêt à l'emploi."/>
 		<meta name="viewport" content="initial-scale=1,minimum-scale=1,width=device-width">
+		
+		<!-- Icones pour le site --> 
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+		<meta name="apple-mobile-web-app-title" content="Slim CSS Framework">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="img/favicons/mstile-144x144.png">
+		<meta name="application-name" content="Slim CSS Framework">
 
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="css/slim-icons.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="css/slim.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
-
-		<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-		<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-		<!--[if IE 8]> <html class="no-js lt-ie9"><![endif]-->
-		<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
-		<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-
 
 	</head>
 
