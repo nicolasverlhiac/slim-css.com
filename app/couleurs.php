a8b532eb523e2093c056ebbf85aef3f33aef6a84
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2014-11-16 00:32:34 +0100

    ADD -Couleur

---
+++ app/couleurs.php
@@ -0,0 +1,138 @@
+<?php require_once "header.php"; ?>
+
+			<div class="cols-row">
+
+				<div class="col-20">
+					<?php require_once "sidebar.php" ?>
+				</div>
+
+				<div class="col-80">
+
+					<h2 class="titre-slim">Couleurs</h2>
+					<p>Une palette complète de couleurs additionnelle est fournis dans le framework</p>
+
+					<h3 id="couleurs" class="doc-slim">Couleurs Background et texte <a href="#couleurs" class="doc-link icon-link" title="Ancre de navigation"></a></h3>
+					<p>Pour utiliser ces couleurs en background, prennez la class <code>.bg-xxxxxxxx</code>.</p>
+
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-turquoise centered">.bg-turquoise</div>
+							<div class="bloc-couleur width-100 bg-green-sea centered">.bg-green-sea</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-emerald centered">.bg-emerald</div>
+							<div class="bloc-couleur width-100 bg-nephritis centered">.bg-nephritis</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-peter-river centered">.bg-peter-river</div>
+							<div class="bloc-couleur width-100 bg-belize-hole centered">.bg-belize-hole</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-amethyst centered">.bg-amethyst</div>
+							<div class="bloc-couleur width-100 bg-wisteria centered">.bg-wisteria</div>
+						</div>
+					</div>
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-wet-asphalt centered">.bg-wet-asphalt</div>
+							<div class="bloc-couleur width-100 bg-midnight-blue centered">.bg-midnight-blue</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-sun-flower centered">.bg-sun-flower</div>
+							<div class="bloc-couleur width-100 bg-orange centered">.bg-orange</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-carrot centered">.bg-carrot</div>
+							<div class="bloc-couleur width-100 bg-pumpkin centered">.bg-pumpkin</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-alizarin centered">.bg-alizarin</div>
+							<div class="bloc-couleur width-100 bg-pomegranate centered">.bg-pomegranate</div>
+						</div>
+					</div>
+
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-clouds centered">.bg-clouds</div>
+							<div class="bloc-couleur width-100 bg-silver centered">.bg-silver</div>
+						</div>
+						<div class="col-25">
+							<div class="bloc-couleur width-100 bg-concrete centered">.bg-concrete</div>
+							<div class="bloc-couleur width-100 bg-asbestos centered">.bg-asbestos</div>
+						</div>
+					</div>
+<pre class="prettyprint">
+&lt;div class="bg-turquoise"&gt;
+	Background de ce div en turquoise
+&lt;/div&gt;
+</pre>
+
+					<p>Pour utiliser ces couleurs en coloration de texte, ne prenez que le nom de la couleur <code>.xxxxxxx</code>.</p>
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="width-100 turquoise centered">.turquoise</div>
+							<div class="width-100 green-sea centered">.green-sea</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 emerald centered">.emerald</div>
+							<div class="width-100 nephritis centered">.nephritis</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 peter-river centered">.peter-river</div>
+							<div class="width-100 belize-hole centered">.belize-hole</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 amethyst centered">.amethyst</div>
+							<div class="width-100 wisteria centered">.wisteria</div>
+						</div>
+					</div>
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="width-100 wet-asphalt centered">.wet-asphalt</div>
+							<div class="width-100 midnight-blue centered">.midnight-blue</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 sun-flower centered">.sun-flower</div>
+							<div class="width-100 orange centered">.orange</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 carrot centered">.carrot</div>
+							<div class="width-100 pumpkin centered">.pumpkin</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 alizarin centered">.alizarin</div>
+							<div class="width-100 pomegranate centered">.pomegranate</div>
+						</div>
+					</div>
+
+					<div class="cols-row">
+						<div class="col-25">
+							<div class="width-100 clouds centered">.clouds</div>
+							<div class="width-100 silver centered">.silver</div>
+						</div>
+						<div class="col-25">
+							<div class="width-100 concrete centered">.concrete</div>
+							<div class="width-100 asbestos centered">.asbestos</div>
+						</div>
+					</div>
+
+
+					<pre class="prettyprint">
+&lt;div class="turquoise"&gt;
+	Couleur du texte de ce div en turquoise
+&lt;/div&gt;</pre>
+
+
+
+				</div>
+			</div>
+				
+		</div>
+		
+		<footer>
+			<?php require_once "footer.php" ?>
+		</footer>
+
+	</body>
+</html>
+
