215164f36ec9db873323db5db6669f364714dc17
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2014-11-16 00:33:01 +0100

    ADD - Demonstration page

---
+++ app/demonstration.php
@@ -0,0 +1,39 @@
+<?php require_once "header.php"; ?>
+
+			<div class="cols-row">
+				<div class="col-100">
+					
+					<div class="cols-row">
+						<div class="col-60">
+							<p class="lead">Slim est née d'un besoin simple : Un Framework CSS très léger et modulable mais qui comporterait tous les outils nécessaires pour faire des sites web rapidement. 
+								Bien évidement, il devait être <strong>responsive</strong>, et gérer l'<strong>interopérabilité</strong> entre les navigateurs récents et vieux. Et surtout, facile à prendre en main.
+								Voilà comment le framework CSS Slim se retrouve maintenant entre vos mains. </p>
+
+							<p class="lead">Slim est un projet vivant et Open Source, qui évolue au fur et à mesure. 
+								Actuellement, il possède un certain nombre de fonctionnalités qui permettent de commencer un projet web stable. 
+								Dans un but constant d'améliorartion du framework, suivez son évolution grâce aux <a href="changelog.php">ChangeLogs</a> (journal des modifications).</p>
+						</div>
+						<div class="col-40 centered">
+
+							<img src="img/logo-presentation-slim.png" alt="">
+						</div>
+					</div>
+					<div class="cols-row">
+						<div class="bloc-profil col-60">
+							<img src="img/nicolas-verlhiac-profil.png" alt="">
+							<h3>Nicolas Verlhiac</h3>
+							<h3 class="soustitre">Créateur de Slim & Webdesigner</h3>
+							<p>Je nourris l'idée qu'un jour, le web sera un lieu accessible à tous et répondra aux besoins de chacun. - <a href="http://nicolasverlhiac.fr" target="_blank">website</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+				
+		</div>
+		
+		<footer>
+			<?php require_once "footer.php" ?>
+		</footer>
+
+	</body>
+</html>
