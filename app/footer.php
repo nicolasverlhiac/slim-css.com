694d8c95ebc4b89cd3dd61ea30b1de550e2882ee
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2015-01-03 14:53:46 +0100

    ADD - Footer

--- app/footer.php
+++ app/footer.php
@@ -1,3 +1,16 @@
+
+	<div class="container begin end padding-20">
+		<div class="cols-row end">
+			<div class="col-50 col-centre centered">
+				<hr>
+				<p>
+					<a href="mentions-legales<?php echo $extension; ?>">Mentions Légales</a>
+					<br>
+					Copyright <?php echo date("Y"); ?> - Slim CSS Framework 
+				</p>
+			</div>
+		</div>
+	</div>
 <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
 		<script src="js/jquery.min.js"></script>
 		<script src="js/main.js"></script>
