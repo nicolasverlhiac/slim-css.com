a31fc01865d86c2cdfc4e1e2d2a0572f4bd1f650
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2015-01-03 14:54:14 +0100

    UP - template

--- app/templates.php
+++ app/templates.php
@@ -1,5 +1,18 @@
 <?php require_once "header.php"; ?>
+			<div class="cols-row">
+				<div class="col-100 centered">
+					<p class="lead">Chaque template peut être téléchargé indépendamment (récupération du fichier HTML seulement). 
+						<br>
+						Pour télécharger un projet complet, retrouvez tous les templates dans le <a style="margin-top:50px;" href="telechargement<?php echo $extension; ?>">Starter Pack</a>.
+					</p>
+				</div>
+			</div>
 
+			<div class="cols-row">
+				<div class="col-50 col-centre">
+					<hr>
+				</div>
+			</div>
 			<div class="cols-row">
 				<div class="col-100">
 					
@@ -21,14 +34,14 @@
 									</p>
 									<p>
 										<a href="dl-file.php?url=https://raw.githubusercontent.com/nicolasverlhiac/Slim-CSS-Starter-Pack/master/index.html&nom=One-Page-Slim.html" class="btn moyen btn-green upper">Télécharger <span class="icon-download"></span></a>
-										<a href="templates/template-01<?php echo $extension; ?>" class="btn moyen btn-blue upper">Demo</a>
+										<a href="templates/template-01<?php echo $extension; ?>" class="btn moyen btn-blue upper" target="_blank">Demo</a>
 									</p>	
 								</div>
 							</div>
 						</div>
 						<div class="col-50">
-							<a href="templates/template-01<?php echo $extension; ?>">
-								<img src="img/template-1.jpg" alt="">
+							<a href="templates/template-01<?php echo $extension; ?>" target="_blank">
+								<img src="img/template-1.jpg" alt="" >
 							</a>
 						</div>
 					</div>
