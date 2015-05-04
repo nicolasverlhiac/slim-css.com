5440ba40eaba9dc23ae8c9c035f19f5a8fc6f496
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2015-05-04 11:26:18 +0200

    ADD - French Traduction

---
+++ lang/en.php
@@ -0,0 +1,18 @@
+<?php
+
+  /********************************
+   * SLIM CSS FRAMEWORK
+   * English Language File
+   ********************************/       
+
+  $LANG = array_merge($LANG, array( 
+
+      // Menu TOP
+      'menu_accueil' => 'Home',
+      'menu_presentation' => 'Presentation',
+      'menu_documentation' => 'Documentation',
+      'menu_template' => 'Templates',
+      'menu_telechargement' => 'Download SLIM'
+      
+    ));
+?>
